08cf3104df2445111bd1ccf9964d1ade5e102b74
Gooblicraft <148702245+gooblicraft@users.noreply.github.com>
2025-04-10 22:40:57 +0800

    Added methods "Not complete", added new window when clicked

--- Beta/main.py
+++ Beta/main.py
@@ -1,10 +1,22 @@
 from tkinter import *
-
+from tkinter import ttk
+import time
 #Methods
 def inGuess():
     label1.config(text=f"You are guessing number: {entry1.get()}")    #may ginawa akong experiment (dikopa sinama), simple lang pero nakaka-bano (Maganda sya sa calculator)
     label1.pack(pady=10)
-    
+
+def start_progress():
+    pass
+
+def fill_progress():
+    pass
+
+def progressWindow():
+    toplevel = Toplevel(mainWindow)
+    toplevel.geometry('300x150')
+    toplevel.title('Prompt')
+
 #Mind reader program by Ed and Jayvee
 mainWindow = Tk()
 mainWindow.geometry('300x150')
@@ -19,8 +31,9 @@ entry1 = Entry(mainWindow, width=20)    #Wala pang command or method for getting
 entry1.pack(pady=10)
 entry1.insert(5, '')
 
-button1 = Button(mainWindow, text='Check my Guess', command=inGuess)
+button1 = Button(mainWindow, text='Check my Guess', command=progressWindow)
 button1.pack(pady=10)
+
 mainWindow.mainloop()
 
-#Kulang nalang ng progress bar at multiple window
+#Kulang nalang ng progress bar at dapat nasa front sya ng mainwindow
