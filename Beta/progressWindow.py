199ab9540706f8f8f2a1cad4408607bfb245d651
Jabiii1 <jayveegarcia018@gmail.com>
2025-04-11 17:27:51 +0800

    sync

--- Beta/progressWindow.py
+++ Beta/progressWindow.py
@@ -8,18 +8,18 @@ def progressWindow():
     laodingWindow.title('Guessing')
 
     icon = PhotoImage(file="C:\\Users\\jayve\\Documents\\Jayvee_Files\\New folder\\Mind-Reader-Project\\media_files\\Question_mark.png")
-    loadingWindow.iconphoto(False, icon)
+    loadingWindow.iconphoto(False, icon) 
     
     def stopLoad():
-        loading.stop()
+        loadingWindow.stop()
         laodingWindow.destroy()
     
     toplvllabel = Label(laodingWindow, text="Reading the Users Mind...")
     toplvllabel.pack(pady=15)
     
-    loading = ttk.Progressbar(laodingWindow,mode="determinate", length=100)
-    loading.pack(pady=3)
-    loading.start()
+    loadingWindow = ttk.Progressbar(laodingWindow,mode="determinate", length=100)
+    loadingWindow.pack(pady=3)
+    loadingWindow.start()
     
     laodingWindow.after(5788, stopLoad)
     laodingWindow.lift()
