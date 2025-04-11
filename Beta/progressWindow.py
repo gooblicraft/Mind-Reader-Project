1232fa59177b0e513b424e11d5ad8adea88ff714
gooblicraft <24crafter05@gmail.com>
2025-04-11 17:53:21 +0800

    tweaks and pagayos ng bug sa icon

--- Beta/progressWindow.py
+++ Beta/progressWindow.py
@@ -9,7 +9,7 @@ laodingWindow.title('Guessing')
 laodingWindow.lift()
 laodingWindow.resizable(False,False)    #Not resizable
 
-icon = PhotoImage(file="Beta\\media_files\\Question_mark.png")  #Need maayos kase dinalabas yung icon
+icon = PhotoImage(file="Beta\\media_files\\Question_mark.png")
 laodingWindow.iconphoto(False, icon)
     
 def stopLoad():
